f5517837a81eb6402a6e7d1fd1ea70a85f66fd40
Aryan-Kumr <152487056+Aryan-Kumr@users.noreply.github.com>
2024-07-09 01:22:20 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,49 +1,39 @@
 #include <iostream>
 #include <queue>
+#include <vector> 
 using namespace std;
-
-void bfs(vector<int>adj[], int StartingNode, int v) {
-    vector<int> vis(v,0);
-    vector<int> ans;
+vector<int> ans;
+void BFS(vector<int> adj[], int startNode, int v) {
+    vector<int> vis(v, 0);
     queue<int> q;
-    q.push(StartingNode);
-    vis[StartingNode] = 1;
+    q.push(startNode);
+    vis[startNode] = 1;
     while(!q.empty()) {
         int node = q.front();
         q.pop();
         ans.push_back(node);
-        for (int i = 0; i < adj[node].size(); ++i) {
-            int neighbor = adj[node][i];
-            if (!vis[neighbor]) {
-                vis[neighbor] = 1;  // Mark neighbor as visited
-                q.push(neighbor);
+        for(auto it: adj[node]) {
+            if(!vis[it]) {
+                q.push(it);
+                vis[it] = 1;  
             }
-        } 
-    }
-    cout<<"bfs traversal"<<endl;
-    for(int i = 0; i < ans.size(); i++) {
-        cout<<ans[i]<<" ";
+        }
     }
 }
 int main()
 {
-    vector<int>adj[5];
-    adj[0].push_back(1);  // Node 0 is connected to node 1
-    adj[0].push_back(2);  // Node 0 is also connected to node 2
-    adj[1].push_back(3);  // Node 1 is connected to node 3
-    adj[2].push_back(4);  // Node 2 is connected to node 4
- 
-    
-    for (int i = 0; i < 5; ++i) {
-        cout << "Adjacency list of node " << i << ": ";
-        for (int j = 0; j < adj[i].size(); ++j) {
-            cout << adj[i][j] << " ";
-        }
-        cout << endl;
+    vector<int> adj[5];   // v = 5
+    adj[0].push_back(1);                    //            0 
+    adj[0].push_back(2);                   //           | | |
+    adj[0].push_back(3);                  //            1  2 3 
+    adj[1].push_back(0);                //                 |
+    adj[2].push_back(0);             //                    4
+    adj[2].push_back(4);
+    adj[3].push_back(0);
+    adj[4].push_back(2);
+    BFS(adj, 0, 4);
+    for(auto it: ans) {
+        cout<<it<<" ";
     }
-    
-    bfs(adj, 0, 5);
-
-
     return 0;
 }
